22e321790b498d0fbbf48b25a95c25ce507fb563
HossamElshahawy <hossamelshahawy13@gmail.com>
2023-10-02 22:43:32 +0300

    prepare Database Connection

---
+++ admin/includes/database.php
@@ -0,0 +1,45 @@
+<?php
+
+class Database{
+
+    public $connection ;
+
+    public function __construct()
+    {
+        $this->open_db_connection();
+    }
+
+    function open_db_connection()
+    {
+        try {
+            $this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        }catch (Exception $e){
+        die('Connection Failed '. $this->connection->connect_error);
+        }
+    }
+
+    function query($sql)
+    {
+        try {
+            $result = $this->connection->query($sql);
+            return $result;
+        }catch (Exception $e){
+            echo $e->getMessage();
+        }
+    }
+
+    private function confirm_query($result)
+    {
+        if (!$result){
+            die("Query Failed".$this->connection->error);
+        }
+    }
+
+    public function escape_string($string){
+        $escaped_string = $this->connection->real_escape_string($string);
+        return $escaped_string;
+    }
+
+
+}
+$database = new Database();
